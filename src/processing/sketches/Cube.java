659608f519981b50f65848fc2e1e5bb28397f887
Kyle Kovalchick <Kovalchickzz@gmail.com>
2021-10-20 20:24:21 -0500

    Can now fly cube around

---
+++ src/processing/sketches/Cube.java
@@ -0,0 +1,79 @@
+package processing.sketches;
+
+import processing.core.PApplet;
+import processing.core.PVector;
+
+public class Cube {
+    private Quaternion rotation = new Quaternion();
+    private PVector pos;
+    private PVector velocity = new PVector(0,1,0);
+
+    PVector[] points = new PVector[9];
+
+    PApplet applet = Main.sketch;
+
+
+    public Cube(PVector pos) {
+        this.pos = pos;
+
+        points[0] = new PVector(1,1,1);
+        points[1] = new PVector(-1,1,1);
+        points[2] = new PVector(-1,-1,1);
+        points[3] = new PVector(1,-1,1);
+
+        points[4] = new PVector(1,1,-1);
+        points[5] = new PVector(-1,1,-1);
+        points[6] = new PVector(-1,-1,-1);
+        points[7] = new PVector(1,-1,-1);
+
+        points[8] = new PVector(0,4,0);
+    }
+
+    PVector lastMousePos = new PVector();
+
+
+    public void update(){
+        float[][] mat = rotation.toMatrix();
+
+        pos.add(MathUtil.MultiMat(velocity, mat));
+        if(applet.mousePressed){
+            Quaternion mouseRot = new Quaternion().fromEuler(lastMousePos.x - applet.mouseX, applet.mouseY - lastMousePos.y, 0);
+            rotation = mouseRot.multi(rotation);
+        }
+        lastMousePos.x = applet.mouseX;
+        lastMousePos.y = applet.mouseY;
+    }
+
+
+    public void draw(){
+
+        float[][] mat = rotation.toMatrix();
+
+        int i = 0;
+        for (PVector point: points) {
+            PVector p = MathUtil.MultiMat(point, mat);
+
+            float color = p.z;
+            p.mult(60);
+            p.add(pos);
+
+            applet.stroke((color + 1) * 255/2, (color + 1) * 255/2,255);
+            applet.strokeWeight(1);
+
+            if(i == 8){
+
+                applet.line(pos.x, pos.y, p.x,p.y);
+            }
+
+            applet.strokeWeight(10);
+            applet.point(p.x, p.y);
+
+            i++;
+        }
+
+
+    }
+
+
+
+}
--- src/processing/sketches/Main.java
+++ src/processing/sketches/Main.java
@@ -1,5 +1,6 @@
 package processing.sketches;
 
+import org.checkerframework.checker.units.qual.C;
 import processing.core.PApplet;
 import processing.core.PVector;
 
@@ -8,14 +9,13 @@ import java.util.Comparator;
 
 public class Main extends PApplet {
     public static PApplet sketch;
+    Quaternion rot = new Quaternion().fromEuler(1,1,1);
 
-    Quaternion quaternion = new Quaternion().fromEuler(45,0,0);
-
-    Quaternion rot = new Quaternion().fromEuler(1,1,0);
-
+    Cube cube;
 
 
-    PVector[] points = new PVector[12];
+
+    PVector[] points = new PVector[4];
 
     public void settings() {
         sketch = this;
@@ -24,82 +24,20 @@ public class Main extends PApplet {
 
     public void setup() {
         background(0);
-
-        quaternion.normalize();
-
-        points[0] = new PVector(1,1,1);
-        points[1] = new PVector(-1,1,1);
-        points[2] = new PVector(-1,-1,1);
-        points[3] = new PVector(1,-1,1);
-
-        points[4] = new PVector(1,1,-1);
-        points[5] = new PVector(-1,1,-1);
-        points[6] = new PVector(-1,-1,-1);
-        points[7] = new PVector(1,-1,-1);
-
-        points[8] = new PVector();
-        points[9] = new PVector(4,0,0);
-        points[10] = new PVector(0,4,0);
-        points[11] = new PVector(0,0,4);
+        cube = new Cube( new PVector(width/2f, height/2f));
     }
 
-    PVector center;
-    PVector lastMousePos = new PVector();
+
 
     public void draw() {
         clear();
-        PVector pos = new PVector(width/2f, height/2f/* + sin(MathUtil.degToRad(frameCount % 360))*100*/);
-        float[][] mat = quaternion.toMatrix();
 
-        int i = 0;
-        for (PVector point: points) {
-            PVector p = MathUtil.MultiMat(point, mat);
+        cube.update();
 
-            float color = p.z;
-            p.mult(60);
-            p.add(pos);
-
-            stroke((color + 1) * 255/2, (color + 1) * 255/2,255);
-            strokeWeight(1);
-
-            if(i == 8)
-                center = p;
-            if(i == 9) {
-                stroke(255, 0, 0);
-                line(center.x, center.y, p.x, p.y);
-            }
-            if(i == 10) {
-                stroke(0, 255, 0);
-                line(center.x, center.y, p.x, p.y);
-            }
-            if(i == 11) {
-                stroke(0, 0, 255);
-                line(center.x, center.y, p.x, p.y);
-            }
-
-            strokeWeight(10);
-
-
-            point(p.x, p.y);
-
-            i++;
-        }
-
-        if(mousePressed){
-            Quaternion mouseRot = new Quaternion().fromEuler(lastMousePos.x - mouseX, mouseY - lastMousePos.y, 0);
-            quaternion = mouseRot.multi(quaternion);
-
-        }else {
-            quaternion  = rot.multi(quaternion);
-        }
-        lastMousePos.x = mouseX;
-        lastMousePos.y = mouseY;
-
-        //quaternion  = rot2.multi(quaternion);
-
-
+        cube.draw();
 
     }
+
 
     public static void main(String... args) {
         PApplet.main("processing.sketches.Main");
